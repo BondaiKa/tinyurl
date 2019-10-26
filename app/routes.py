f3f78d6bca6c206d97cd8c860b38ebacac8faea4
Karim <karim.safiullin@kzn.akvelon.com>
2019-10-26 16:08:09 +0300

    add check valid url on server

--- app/routes.py
+++ app/routes.py
@@ -2,19 +2,27 @@ from flask import render_template, redirect, url_for, abort
 from app import app, redis
 from flask import request
 from .compression import CompressionLink
+import re
 
 comp = CompressionLink()
 
+regex = re.compile(
+    r'^(?:http|ftp)s?://'  # http:// or https://
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+    r'(?::\d+)?'  # optional port
+    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
 
 @app.errorhandler(404)
-def page_not_found():
+def page_not_found(error):
     return render_template('404.html', title='404'), 404
 
 
 def create_short_link():
     full_link, short_link = None, None
-    #TODO: need check valid url
-    if request.method == 'POST':
+
+    if request.method == 'POST' and re.match(regex, request.form['full_link']):
         full_link = request.form['full_link']
         redis.incr('linked_id')
         counter = int(redis.get('linked_id'))
