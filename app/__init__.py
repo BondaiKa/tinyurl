e6a551ec4a8613d1d46bf5a887f35999591bddae
Karim <karim.safiullin@akvelon.com>
2020-06-24 22:54:47 +0300

    fix circular import issue

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
-from app import routes
-from flask import Flask
-from config import Config
 import redis
-
+from config import Config
+from flask import Flask
 
 app = Flask(__name__)
 redis = redis.Redis(host='redis', port=6379, decode_responses=True)
 
 app.config.from_object(Config)
+
+from app import routes
